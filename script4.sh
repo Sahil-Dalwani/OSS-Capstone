3903dbb3b8af7395f0de22a5807693feb1fa83a4
Sahil-Dalwani <sahil.24bsa10350@vitbhopal.ac.in>
2026-03-31 02:33:22 +0530

    Create script4.sh for log file analysis
    Add a log file analyzer script that counts keyword occurrences.

--- script4.sh
+++ script4.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+LOGFILE="testlog.txt"
+KEYWORD="error"
+
+COUNT=0
+
+echo "=============================="
+echo "       Log File Analyzer"
+echo "=============================="
+
+while read LINE
+do
+    if echo "$LINE" | grep -i "$KEYWORD" > /dev/null
+    then
+        COUNT=$((COUNT+1))
+    fi
+done < $LOGFILE
+
+echo "Keyword '$KEYWORD' found $COUNT times."
+
+echo "Recent occurrences:"
+grep -i "$KEYWORD" $LOGFILE | tail -5
