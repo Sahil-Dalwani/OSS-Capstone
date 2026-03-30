1b03949968fa5a433b13726d86e9f112f341200f
Sahil-Dalwani <sahil.24bsa10350@vitbhopal.ac.in>
2026-03-31 02:32:35 +0530

    Add FOSS Package Inspector script for Python3

--- script2.sh
+++ script2.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+PACKAGE="python3"
+
+echo "================================="
+echo "        FOSS Package Inspector"
+echo "================================="
+
+if dpkg -s $PACKAGE &> /dev/null
+then
+    echo "Status: $PACKAGE is installed."
+
+    VERSION=$(python3 --version)
+    echo "Version: $VERSION"
+
+else
+    echo "Status: $PACKAGE is NOT installed."
+fi
+
+echo "================================="
